be6045d716d3a7e4c092aa61a17863ac7537d78c
default <devin.boucher@stu.ocsb.ca>
2025-04-30 17:23:52 +0000

    made code

--- js/script.js
+++ js/script.js
@@ -7,15 +7,27 @@
 'use strict'
 
 // eslint-disable-next-line no-unused-vars
-function checkNumber () {
-  let age = parseFloat(document.getElementById('age').value)
-  const day = document.getElementById('day').value
+function checkTriangleType() {
+  const lengthA = parseFloat(document.getElementById('sideA').value)
+  const lengthB = parseFloat(document.getElementById('sideB').value)
+  const lengthC = parseFloat(document.getElementById('sideC').value)
+  let typeOfTriangle 
 
-  if (((age > 12) && (age < 21)) || (day === 'Tuesday') || (day === 'Thursday')) {
-    age = ('You get a discount at the museum!')
+const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
+const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
+const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+
+const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+
+  if (sumOfAngles !== 180) {
+    typeOfTriangle = ('These sides do not form a triangle.')
+  } else if (lengthA === lengthB && lengthB === lengthC) {
+    typeOfTriangle = ('This is an equilateral triangle')
+  } else if (lengthA === lengthB || lengthB === lengthC || lengthA === lengthC) {
+    typeOfTriangle = ('This is an isosceles triangle.')
   } else {
-    age = ('You must pay full price at the museum.')
+    typeOfTriangle = ('This is a scalene triangle.')
   }
 
-  document.getElementById('result').innerHTML = `<p><strong>${age}</strong></p>`
+  document.getElementById('result').innerHTML = `<p><strong>${typeOfTriangle}</strong></p>`
 }
