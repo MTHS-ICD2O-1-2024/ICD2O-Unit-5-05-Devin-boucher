e0f55f4974913c473156c732be58d4fd8f85a081
default <devin.boucher@stu.ocsb.ca>
2025-05-01 16:16:35 +0000

    fixed formating

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@ function checkTriangleType() {
   const lengthA = parseFloat(document.getElementById('sideA').value)
   const lengthB = parseFloat(document.getElementById('sideB').value)
   const lengthC = parseFloat(document.getElementById('sideC').value)
-  let typeOfTriangle 
+  let typeOfTriangle
 
-const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
-const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
-const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+  const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
+  const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI)
+  const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
 
-const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  const sumOfAngles = Number(angleA.toFixed(2)) + Number(angleB.toFixed(2)) + Number(angleC.toFixed(2))
 
   if (sumOfAngles !== 180) {
     typeOfTriangle = ('These sides do not form a triangle.')
   } else if (lengthA === lengthB && lengthB === lengthC) {
-    typeOfTriangle = ('This is an equilateral triangle')
+    typeOfTriangle = ('This is an equilateral triangle.')
   } else if (lengthA === lengthB || lengthB === lengthC || lengthA === lengthC) {
     typeOfTriangle = ('This is an isosceles triangle.')
   } else {
